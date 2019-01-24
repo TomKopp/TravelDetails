38fd7e5ef502386b2010d77e0ca09cd71520471b
Tom Kopp <witzbould@live.com>
2019-01-24 14:04:39 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.js'
+    , mode: 'development'
+    , module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/
+                , exclude: /(node_modules|bower_components)/
+                , loader: 'babel-loader'
+                , options: { presets: ['@babel/preset-react'] }
+            }
+            , {
+                test: /\.css$/
+                , use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    }
+    , resolve: {
+        extensions: [
+            '*'
+            , '.js'
+            , '.jsx'
+        ]
+    }
+    , output: {
+        path: path.resolve(__dirname, 'dist/')
+        , publicPath: '/dist/'
+        , filename: 'main.js'
+    }
+    , devServer: {
+        contentBase: path.join(__dirname, 'public/')
+        , port: 8080
+        , publicPath: 'http://localhost:8080/dist/'
+        , hotOnly: true
+    }
+    , plugins: [new webpack.HotModuleReplacementPlugin()]
+};
