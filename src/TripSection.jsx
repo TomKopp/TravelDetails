08f3883cfa049d70a03e4cb1833442ad97363814
Tom Kopp <witzbould@live.com>
2019-01-31 14:00:20 +0100

    use accommodation, activity, medialist build trip section ard

--- src/TripSection.jsx
+++ src/TripSection.jsx
@@ -1,18 +1,43 @@
+import { Button, Grid, Paper, Typography } from '@material-ui/core';
+import { FlightLand, FlightTakeoff, Hotel, Place, Star } from '@material-ui/icons';
+import Accommodation from './Accommodation';
+import Activity from './Activity';
+import MediaList from './MediaList';
 import React from 'react';
 
-// export default class TripSection extends Component {}
+export default ({ id, location, startDate, endDate, content, mediaList, rating, transport, hotel, activities }) => {
+    const title = content.shift();
 
-// Rating component
-// Transportation component
-// Accomodation component
+    return <Paper className="section" component="article">
+        <Grid container spacing={16} className="section-summary">
+            <Grid item xs={12} sm={5}>
+                <MediaList children={mediaList} />
+            </Grid>
+            <Grid item container direction="column" xs={12} sm={7}>
+                <Grid item>
+                    <Typography gutterBottom variant="h4" component="h1">{title}</Typography>
+                    <Typography gutterBottom>
+                        {startDate && <span className="ico-string"><FlightTakeoff />{startDate.toDateString()}</span>}
+                        {endDate && <span className="ico-string"><FlightLand />{endDate.toDateString()}</span>}
+                    </Typography>
+                    <Typography gutterBottom>{hotel && <span className="ico-string"><Hotel />{hotel.name}</span>}</Typography>
+                    <Typography gutterBottom>{rating && <span className="ico-string"><Star />Rating: {rating}</span>}</Typography>
+                </Grid>
+                <Grid item>
+                    <Typography gutterBottom>{location && <span className="ico-string"><Place />{location}</span>}</Typography>
+                </Grid>
+                <Grid item>
+                    <Typography gutterBottom>
+                        {content.map((el, key) => <Typography key={key} gutterBottom component="span">{el}</Typography>)}
+                    </Typography>
+                </Grid>
+                <Grid item container justify="flex-end">
+                    <Button variant="contained" color="primary">Change</Button>
+                </Grid>
+            </Grid>
+        </Grid>
 
-export default ({ content, mediaList, location, rating }) => {
-    const [contentFirst, ...contentRest] = content;
-
-    return <section className="trip-section">
-        <h1>{contentFirst}</h1>
-        {rating && <b className="rating">Rating: {rating}</b>}
-        {location && <p className="location">{location}</p>}
-        {contentRest && <p className="content">{contentRest.map((el, key) => <span key={key}>{el}</span>)}</p>}
-    </section>;
+        <Accommodation {...hotel} />
+        {activities.map((el, key) => <Activity key={key} {...el} />)}
+    </Paper>;
 };
