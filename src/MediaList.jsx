09f013f34caecaf9f62834b3408a14498e2987b8
Tom Kopp <witzbould@live.com>
2019-02-05 16:07:34 +0100

    break up media list into reusable parts

--- src/MediaList.jsx
+++ src/MediaList.jsx
@@ -1,12 +1,16 @@
 import { GridList, GridListTile, GridListTileBar } from '@material-ui/core';
 import React from 'react';
 
-export default ({ children }) => <GridList className="media-list" cols={1} cellHeight={'auto'}>
-    {children.map(({ type, src, description }, key) => {
-        const Content = type ? type : 'img';
-        return <GridListTile key={key} className="media-list-tile">
-            <Content className="media-list-tile-media" src={src} />
-            {description && <GridListTileBar className="media-list-tile-description" title={description} />}
-        </GridListTile>;
-    })}
+export const MediaObject = ({ type, src, description }, key) => {
+    const Content = type ? type : 'img';
+    return <GridListTile key={key} className="media-list-tile">
+        <Content className="media-list-tile-media" src={src} />
+        {description && <GridListTileBar className="media-list-tile-description" title={description} />}
+    </GridListTile>;
+};
+
+export const MediaList = ({ children }) => <GridList className="media-list" cols={1} cellHeight={'auto'}>
+    {children.map(MediaObject)}
 </GridList>;
+
+export default MediaList;
